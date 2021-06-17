9ecffb0b19f0b6abaa080ea7e730befaaaf2ddb4
Dennis Lu <ludennis0606@gmail.com>
2021-06-17 17:01:11 +0800

    install_packages.sh: added curl

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository universe
-sudo apt install gnome-tweak-tool
-sudo apt-get install shutter vim git tmux htop
+sudo apt install gnome-tweak-tool -y
+sudo apt-get install shutter vim git tmux htop curl -y
